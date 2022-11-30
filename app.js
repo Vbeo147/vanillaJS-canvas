3bffa6ad31b66dc1a56894eec839ba4d77031cc5
Vbeo147 <vbeo147@gmail.com>
2022-11-30 18:50:36 +0900

    [Code] More Update

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
-canvas.height = 800;
+canvas.height = 600;
+ctx.lineWidth = lineWidth.value;
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.fill();
-ctx.beginPath();
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
-ctx.fillStyle = "red";
-ctx.fill();
+let isPainting = false;
+
+function onMove(e) {
+  if (isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(e) {
+  ctx.lineWidth = e.target.value;
+}
+
+function onColor(e) {
+  const CurrentTarget = e.target;
+  if (e.target.dataset.color) {
+    ctx.strokeStyle = CurrentTarget.dataset.color;
+    ctx.fillStyle = CurrentTarget.dataset.color;
+    color.value = CurrentTarget.dataset.color;
+  } else {
+    ctx.strokeStyle = CurrentTarget.value;
+    ctx.fillStyle = CurrentTarget.value;
+  }
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColor);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColor));
